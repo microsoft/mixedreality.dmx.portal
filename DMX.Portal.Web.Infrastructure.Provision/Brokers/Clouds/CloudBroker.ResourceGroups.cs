0fc4bc7f359c5badaba916946a13eebdc48df58f
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 14:43:44 -0700

    Implement ResourceGroup broker

---
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.ResourceGroups.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<IResourceGroup> CreateResourceGroupAsync(string resourceGroupName)
+        {
+            return await this.azure.ResourceGroups
+                .Define(resourceGroupName)
+                .WithRegion(region: Region.USCentral)
+                .CreateAsync();
+        }
+
+        public async ValueTask<bool> CheckResourceGroupExistsAsync(string resourceGroupName) =>
+            await this.azure.ResourceGroups.ContainAsync(resourceGroupName);
+
+
+    }
+}
