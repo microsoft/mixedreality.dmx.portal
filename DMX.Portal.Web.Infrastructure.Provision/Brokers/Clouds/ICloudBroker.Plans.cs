579ee2ea4fa668277e19136828f8342b2a84d4a3
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 14:43:01 -0700

    Implement Plan Broker

---
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Plans.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IAppServicePlan> CreatePlanAsync(string planName, IResourceGroup resouceGroup);
+    }
+}
