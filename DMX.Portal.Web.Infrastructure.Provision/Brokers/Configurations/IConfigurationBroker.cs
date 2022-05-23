e8df669165dc0f34f71b45e4f957fd59546c9c48
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 14:53:17 -0700

    Implement configuration broker

---
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Configurations/IConfigurationBroker.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Infrastructure.Provision.Models.Configurations;
+
+namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Configurations
+{
+    public interface IConfigurationBroker
+    {
+        public CloudManagementConfiguration GetConfiguration();
+    }
+}
