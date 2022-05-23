41b5a82fa6ec057b38f49a7552bd56e548d76324
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 14:44:25 -0700

    Implement storage broker

---
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Storages.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Portal.Web.Infrastructure.Provision.Models.Storages;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Sql.Fluent;
+
+namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker
+    {
+        public async ValueTask<ISqlServer> CreateSqlServerAsync(string sqlServerName, IResourceGroup resourceGroup)
+        {
+            return await azure.SqlServers
+                .Define(sqlServerName)
+                .WithRegion(Region.USCentral)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithAdministratorLogin(this.adminName)
+                .WithAdministratorPassword(this.adminAccess)
+                .CreateAsync();
+        }
+
+        public async ValueTask<ISqlDatabase> CreateSqlDatabaseAsync(string sqlDatabaseName, ISqlServer sqlServer)
+        {
+            return await azure.SqlServers.Databases
+                .Define(sqlDatabaseName).
+                WithExistingSqlServer(sqlServer)
+                .CreateAsync();
+        }
+
+        public SqlDatabaseAccess GetDatabaseAccess()
+        {
+            return new SqlDatabaseAccess
+            {
+                AdminAccess = this.adminAccess,
+                AdminName = this.adminName
+            };
+        }
+    }
+}
