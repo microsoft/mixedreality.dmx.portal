2d543ea5210bfe691ab758b4c1d7cdae1bc35ac0
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 14:47:48 -0700

    Implement logging broker

---
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        public void LogActivity(string message) => Console.WriteLine(message);
+    }
+}
