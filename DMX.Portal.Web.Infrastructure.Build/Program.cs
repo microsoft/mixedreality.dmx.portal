5c876a5a1e34b61a906691459ea00a1b1554cf76
Geetika Jain <geetikajain@microsoft.com>
2022-05-23 22:27:27 -0700

    Add copyrights

--- DMX.Portal.Web.Infrastructure.Build/Program.cs
+++ DMX.Portal.Web.Infrastructure.Build/Program.cs
@@ -1,4 +1,8 @@
-﻿using ADotNet.Clients;
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using ADotNet.Clients;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
 using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
@@ -64,4 +68,4 @@ var githubPipeline = new GithubPipeline
 var aDotNetClient = new ADotNetClient();
 aDotNetClient.SerializeAndWriteToFile(
     githubPipeline,
-    "../../../../.github/workflows/dotnet.yml");
+    path: "../../../../.github/workflows/dotnet.yml");
