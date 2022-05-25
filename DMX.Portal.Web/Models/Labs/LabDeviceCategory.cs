d9aa15a043bffd358d9b1bdf665b65da7c10ba1a
Andrew McClelland <anmcclel@microsoft.com>
2022-05-25 13:48:30 -0700

    Add Labs models

---
+++ DMX.Portal.Web/Models/Labs/LabDeviceCategory.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+namespace DMX.Portal.Web.Models.Labs
+{
+    public enum LabDeviceCategory
+    {
+        Host,
+        Attachment
+    }
+}
