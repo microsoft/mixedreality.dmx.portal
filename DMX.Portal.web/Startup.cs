c41cece932a87661e5e586a9e885ddfbe14b6558
Andrew McClelland <anmcclel@microsoft.com>
2022-05-25 13:23:47 -0700

    CODERUB : Adding missing using statements in Portal.Web startup

--- DMX.Portal.web/Startup.cs
+++ DMX.Portal.web/Startup.cs
@@ -2,12 +2,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace DMX.Portal.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) =>
-            Configuration = configuration;
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
