aeab7ad0083d3be1f12ebf47fbfe1df044e3d194
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-31 10:31:19 -0700

    BROKERS: Implement Logging Broker

---
+++ DMX.Portal.Web/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System;
+
+namespace DMX.Portal.Web.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation (string message);
+        void LogTrace (string message);
+        void LogDebug (string message);
+        void LogWarning (string message);
+        void LogError (Exception exception);
+        void LogCritical (Exception exception);
+    }
+}
