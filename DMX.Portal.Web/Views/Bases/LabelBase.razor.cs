da38caffa9ea073f24f73c528ed4dc4945d0a90d
Geetika Jain <geetsk2194@gmail.com>
2022-06-09 16:19:42 -0700

    CODERUB: Add copyrights

--- DMX.Portal.Web/Views/Bases/LabelBase.razor.cs
+++ DMX.Portal.Web/Views/Bases/LabelBase.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
 
 namespace DMX.Portal.Web.Views.Bases
 {
