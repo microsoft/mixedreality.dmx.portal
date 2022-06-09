3d8afedcc913be2e32d7b2267e7a0ba1e1f6c0a7
Hassan Habib <hassanhabib@live.com>
2022-06-09 10:32:42 -0700

    ShouldRenderLabViewName -> FAIL

---
+++ DMX.Portal.Web.Tests.Unit/Views/LabOverviews/LabOverviewComponentTests.Render.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Reflection.Metadata;
+using Bunit;
+using DMX.Portal.Web.Models.Views.LabViews;
+using DMX.Portal.Web.Views.Components.LabOverviews;
+using FluentAssertions;
+using Force.DeepCloner;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Views.LabOverviews
+{
+    public partial class LabOverviewComponentTests
+    {
+        [Fact]
+        public void ShouldRenderDefaultComponent()
+        {
+            // given . when
+            var initialLabOverviewComponent =
+                new LabOverviewComponent();
+
+            // then
+            initialLabOverviewComponent.LabView.Should().BeNull();
+            initialLabOverviewComponent.LabTitle.Should().BeNull();
+        }
+
+        [Fact]
+        public void ShouldRenderLabViewName()
+        {
+            // given
+            LabView randomLabView = CreateRandomLabView();
+            LabView inputLabView = randomLabView;
+            LabView expectedLabview = inputLabView.DeepClone();
+
+            ComponentParameter inputComponentParameters =
+                ComponentParameter.CreateParameter(
+                    name: nameof(LabOverviewComponent.LabView),
+                    value: inputLabView);
+
+            // when
+            this.renderedLabOverviewComponent =
+                RenderComponent<LabOverviewComponent>(
+                    inputComponentParameters);
+
+            // then
+            this.renderedLabOverviewComponent.Instance.LabView
+                .Should().BeEquivalentTo(expectedLabview);
+
+            this.renderedLabOverviewComponent.Instance.LabTitle.Text
+                .Should().BeEquivalentTo(expectedLabview.Name);
+        }
+    }
+}
---
+++ DMX.Portal.Web/Views/Components/LabOverviews/LabOverviewComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Models.Views.LabViews;
+using DMX.Portal.Web.Views.Bases;
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Components.LabOverviews
+{
+    public partial class LabOverviewComponent
+    {
+        [Parameter]
+        public LabView LabView { get; set; }
+
+        public LargeTitleBase LabTitle { get; set; }
+    }
+}
