e6279ea48d91573548a64d5469e188e74908fe0c
Hassan Habib <hassanhabib@live.com>
2022-06-13 10:14:16 -0700

    BASES: Tables, Rows & Cells

---
+++ DMX.Portal.Web/Views/Bases/TableBase.razor.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public partial class TableBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ DMX.Portal.Web/Views/Bases/TableCellBase.razor.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public partial class TableCellBase : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+    }
+}
