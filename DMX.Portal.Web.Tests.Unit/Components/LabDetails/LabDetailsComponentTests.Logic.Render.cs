f2b3fd9e6aa4fbb23cd5ea9a8dc0c1eaec44ac6b
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-13 10:30:29 -0700

    ShouldHaveDefaultValues -> PASS

---
+++ DMX.Portal.Web.Tests.Unit/Components/LabDetails/LabDetailsComponentTests.Logic.Render.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Bunit;
+using DMX.Portal.Web.Views.Components.LabDetails;
+using FluentAssertions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.LabDetails
+{
+    public partial class LabDetailsComponentTests : TestContext
+    {
+        [Fact]
+        public void ShouldHaveDefaultValues()
+        {
+            // given . when
+            var initialLabDetailsComponent =
+                new LabDetailsComponent();
+
+            // then
+            initialLabDetailsComponent.Lab
+                .Should().BeNull();
+        }
+    }
+}
---
+++ DMX.Portal.Web.Tests.Unit/Components/LabDetails/LabDetailsComponentTests.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Bunit;
+using DMX.Portal.Web.Views.Components.LabDetails;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.LabDetails
+{
+    public partial class LabDetailsComponentTests : TestContext
+    {
+        private IRenderedComponent<LabDetailsComponent> renderedLabDetailsComponent;
+    }
+}
