8751eb1eff3dcba8b7aa20c29dbababfa7dc170a
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-13 10:30:21 -0700

    COMPONENTS: Initialize LabDetailsComponent

---
+++ DMX.Portal.Web/Views/Components/LabDetails/LabDetailsComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Models.Views.LabViews;
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Components.LabDetails
+{
+    public partial class LabDetailsComponent : ComponentBase
+    {
+        [Parameter]
+        public LabView Lab { get; set; }
+    }
+}
