ad1f640e1e6204cbad4729b5f71b07ad6fa4c56f
Hassan Habib <hassanhabib@live.com>
2022-06-13 11:50:42 -0700

    ShouldRenderLabDevicesAndDetails -> FAIL

--- DMX.Portal.Web.Tests.Unit/Components/LabOverviews/LabOverviewComponentTests.Render.cs
+++ DMX.Portal.Web.Tests.Unit/Components/LabOverviews/LabOverviewComponentTests.Render.cs
@@ -2,7 +2,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
 using Bunit;
+using DMX.Portal.Web.Models.Views.LabViews;
+using DMX.Portal.Web.Views.Components.DeviceOverviews;
 using DMX.Portal.Web.Views.Components.LabOverviews;
 using FluentAssertions;
 using Xunit;
@@ -21,6 +25,37 @@ namespace DMX.Portal.Web.Tests.Unit.Components.LabOverviews
             // then
             initialLabOverviewComponent.Lab.Should().BeNull();
             initialLabOverviewComponent.Details.Should().BeNull();
+        }
+
+        [Fact]
+        public void ShouldRenderLabDevicesAndDetails()
+        {
+            // given
+            LabView randomLabView = CreateRandomLabView();
+            LabView inputLabView = randomLabView;
+
+            ComponentParameter inputComponentParameters =
+               ComponentParameter.CreateParameter(
+                   name: nameof(LabOverviewComponent.Lab),
+                   value: inputLabView);
+
+            // when
+            this.renderedLabOverviewComponent =
+                RenderComponent<LabOverviewComponent>(inputComponentParameters);
+
+            // then
+            this.renderedLabOverviewComponent.Instance.Lab
+                .Should().BeEquivalentTo(inputLabView);
+            
+            this.renderedLabOverviewComponent.Instance.Details.Lab
+                .Should().BeEquivalentTo(inputLabView);
+
+            IEnumerable<IRenderedComponent<DeviceOverviewComponent>> allDeviceOverviewComponents =
+                this.renderedLabOverviewComponent.FindComponents<DeviceOverviewComponent>();
+
+            allDeviceOverviewComponents.Select(deviceOverviewComponent =>
+                deviceOverviewComponent.Instance.Device)
+                    .Should().BeEquivalentTo(inputLabView.Devices);
         }
     }
 }
