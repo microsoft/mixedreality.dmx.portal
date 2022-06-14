87f130f88d99103954251633f6a869efc2b2b0ac
Geetika Jain <geetsk2194@gmail.com>
2022-06-14 12:48:29 -0700

    BASES: Add ButtonBase

---
+++ DMX.Portal.Web/Views/Bases/ButtonBase.razor.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public partial class ButtonBase
+    {
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public string ClassName { get; set; }
+
+    }
+}
