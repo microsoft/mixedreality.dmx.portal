79c6a8face66c18f5149693e6a71ecacbc3dee69
Hassan Habib <hassanhabib@live.com>
2022-06-14 14:06:36 -0700

    ShouldRenderTitleStatuses -> PASS

--- DMX.Portal.Web/Views/Components/LabOverviews/LabOverviewComponent.razor.cs
+++ DMX.Portal.Web/Views/Components/LabOverviews/LabOverviewComponent.razor.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
+using DMX.Portal.Web.Models.Views.Components.StatusComponents;
 using DMX.Portal.Web.Models.Views.LabViews;
 using DMX.Portal.Web.Views.Bases;
 using DMX.Portal.Web.Views.Components.LabOverviewDetails;
@@ -21,5 +22,19 @@ namespace DMX.Portal.Web.Views.Components.LabOverviews
         public ContainerBase LabTitleStatusContainer { get; set; }
         public StatusComponent StatusComponent { get; set; }
         public LabOverviewDetailsComponent Details { get; set; }
+        public StatusView LabStatus { get; set; }
+
+        protected override void OnInitialized() =>
+            this.LabStatus = GetLabStatus();
+
+        private StatusView GetLabStatus()
+        {
+            return this.Lab.Status switch
+            {
+                LabStatusView.Available => StatusView.Available,
+                LabStatusView.Offline => StatusView.Offline,
+                _ => StatusView.Reserved
+            };
+        }
     }
 }
