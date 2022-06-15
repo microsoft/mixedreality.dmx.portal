96c70fa226f5c67c4555c2a42324ca8c38018962
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-15 13:23:37 -0700

    ShouldRenderDefaultValues -> PASS

---
+++ DMX.Portal.Web.Tests.Unit/Components/LabOverviewLists/LabOverviewListsComponentTests.Logic.Render.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Views.Components.LabOverviewLists;
+using FluentAssertions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.LabOverviewLists
+{
+    public partial class LabOverviewListsComponentTests
+    {
+        [Fact]
+        public void ShouldRenderDefaultValues()
+        {
+            // given . when
+            var initialRenderedLabOverviewListsComponent = new LabOverviewListsComponent();
+
+            // then
+            initialRenderedLabOverviewListsComponent.Labs
+                .Should().BeNull();
+        }
+    }
+}
---
+++ DMX.Portal.Web.Tests.Unit/Components/LabOverviewLists/LabOverviewListsComponentTests.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Bunit;
+using DMX.Portal.Web.Views.Components.LabOverviewLists;
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.LabOverviewLists
+{
+    public partial class LabOverviewListsComponentTests : TestContext
+    {
+        RenderFragment<LabOverviewListsComponent> renderedLabOverviewListsComponent;
+    }
+}
