4e5f17c093a5b5c6814344f94cc96a0ae75d9aef
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-15 15:43:12 -0700

    ShouldRetrieveAllLabViewsOrderedByNameAsync -> FAIL

--- DMX.Portal.Web.Tests.Unit/Services/Views/LabViews/LabViewServiceTests.Logic.RetrieveAll.cs
+++ DMX.Portal.Web.Tests.Unit/Services/Views/LabViews/LabViewServiceTests.Logic.RetrieveAll.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using DMX.Portal.Web.Models.Labs;
 using DMX.Portal.Web.Models.Views.LabViews;
 using FluentAssertions;
+using Force.DeepCloner;
 using Moq;
 using Xunit;
 
@@ -73,6 +74,73 @@ namespace DMX.Portal.Web.Tests.Unit.Services.Views.LabViews
 
             // then
             actualLabViews.Should().BeEquivalentTo(expectedLabViews);
+
+            this.labServiceMock.Verify(service =>
+                service.RetrieveAllLabsAsync(),
+                    Times.Once);
+
+            this.labServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldRetrieveAllLabViewsOrderedByNameAsync()
+        {
+            // given
+            List<dynamic> randomLabViewProperties =
+                CreateRandomLabViewProperties();
+
+            List<Lab> randomLabs = randomLabViewProperties.Select(item =>
+            {
+                return new Lab
+                {
+                    Id = item.Id,
+                    Name = item.LabName,
+                    Description = item.LabDescription,
+                    Status = item.LabStatus,
+
+                    Devices = ((List<dynamic>)item.Devices).Select(item =>
+                    new LabDevice
+                    {
+                        Name = item.DeviceName,
+                        PowerLevel = item.PowerLevel,
+                        Type = item.LabDeviceType
+                    }).ToList()
+                };
+            }).ToList();
+
+            List<LabView> randomLabViews = randomLabViewProperties.Select(item =>
+            {
+                return new LabView
+                {
+                    Id = item.Id,
+                    Name = item.LabName,
+                    Description = item.LabDescription,
+                    Status = item.LabStatusView,
+                    DmxVersion = item.DmxVersion,
+                    Devices = ((List<dynamic>)item.Devices).Select(item =>
+                    new LabDeviceView
+                    {
+                        Name = item.DeviceName,
+                        PowerLevel = item.PowerLevelView,
+                        Type = item.LabDeviceTypeView
+                    }).ToList()
+                };
+            }).ToList();
+
+            List<Lab> retrievedLabs = randomLabs;
+            List<LabView> expectedLabViews = OrderLabViewsByName(randomLabViews.DeepClone());
+
+            this.labServiceMock.Setup(service =>
+                service.RetrieveAllLabsAsync())
+                    .ReturnsAsync(retrievedLabs);
+
+            // when
+            List<LabView> actualLabViews =
+                await this.labViewService.RetrieveAllLabViewsAsync();
+
+            // then
+            actualLabViews.Should().BeEquivalentTo(expectedLabViews, opt => opt.WithStrictOrdering());
 
             this.labServiceMock.Verify(service =>
                 service.RetrieveAllLabsAsync(),
