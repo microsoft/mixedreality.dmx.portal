6ccfb8f671ee733622245cbe205838de3b48d84f
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-15 13:23:25 -0700

    COMPONENTS: Initialize LabOverviewListsComponent

---
+++ DMX.Portal.Web/Views/Components/LabOverviewLists/LabOverviewListsComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using DMX.Portal.Web.Models.Views.LabViews;
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Components.LabOverviewLists
+{
+    public partial class LabOverviewListsComponent : ComponentBase
+    {
+        public List<LabView> Labs { get; set; }
+    }
+}
