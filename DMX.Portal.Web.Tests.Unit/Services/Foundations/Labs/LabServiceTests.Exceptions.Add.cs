75f18fb22757b7f4783a6dc542da1dc5466d8624
Terry Palomares <tpalomares18@gmail.com>
2022-06-22 13:54:28 -0700

    ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursAndLogItAsync -> FAIL

---
+++ DMX.Portal.Web.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Exceptions.Add.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMX.Portal.Web.Models.Labs;
+using DMX.Portal.Web.Models.Labs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Services.Foundations.Labs
+{
+    public partial class LabServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfCriticalErrorOccursAndLogItAsync(
+            Xeption criticalDependencyException)
+        {
+            // given
+            var someLab = CreateRandomLab();
+
+            var failedLabDependencyException =
+                new FailedLabDependencyException(criticalDependencyException);
+
+            var expectedLabDependencyException =
+                new LabDependencyException(failedLabDependencyException);
+
+            this.dmxApiBrokerMock.Setup(broker =>
+                broker.PostLabAsync(It.IsAny<Lab>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<Lab> addLabTask =
+                this.labService.AddLabAsync(someLab);
+
+            LabDependencyException actualLabDependencyException =
+                await Assert.ThrowsAsync<LabDependencyException>(addLabTask.AsTask);
+
+            // then
+            actualLabDependencyException
+                .Should().BeEquivalentTo(expectedLabDependencyException);
+
+            this.dmxApiBrokerMock.Verify(broker =>
+                broker.GetAllLabsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedLabDependencyException))),
+                        Times.Once);
+
+            this.dmxApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
