01b029aaf4947da44d331abe35923b1aebcb843f
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-22 15:17:15 -0700

    ShouldThrowValidationExceptionOnAddWhenLabIsNullAndLogItAsync -> FAIL

---
+++ DMX.Portal.Web.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Validations.Add.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Portal.Web.Models.Labs;
+using DMX.Portal.Web.Models.Labs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Services.Foundations.Labs
+{
+    public partial class LabServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddWhenLabIsNullAndLogItAsync()
+        {
+            // given
+            Lab nullLab = null;
+            
+            var nullLabException = new NullLabException();
+            var expectedLabValidationException = new LabValidationException(nullLabException);
+
+            // when
+            ValueTask<Lab> addLabTask = this.labService.AddLabAsync(nullLab);
+
+            LabValidationException actualLabValidationException = 
+                await Assert.ThrowsAsync<LabValidationException>(addLabTask.AsTask);
+
+            // then
+            actualLabValidationException.Should().BeEquivalentTo(expectedLabValidationException);
+
+            loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedLabValidationException))),
+                    Times.Once);
+
+            dmxApiBrokerMock.Verify(broker =>
+                broker.PostLabAsync(It.IsAny<Lab>()),
+                    Times.Never);
+
+            dmxApiBrokerMock.VerifyNoOtherCalls();
+            loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DMX.Portal.Web/Models/Labs/Exceptions/LabValidationException.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Portal.Web.Models.Labs.Exceptions
+{
+    public class LabValidationException : Xeption
+    {
+        public LabValidationException(Xeption innerException)
+            : base(message: "Lab validation errors occurred, please try again.",
+                  innerException)
+        { }
+    }
+}
