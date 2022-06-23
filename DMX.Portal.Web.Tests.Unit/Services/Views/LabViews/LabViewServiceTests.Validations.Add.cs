bab405bdeaa9191acc7b98ae8a72af9b88f70471
Terry Palomares <tpalomares18@gmail.com>
2022-06-23 11:42:48 -0700

    ShouldThrowValidationExceptionOnAddIfLabViewIsInvalidAndLogItAsync -> FAIL

--- DMX.Portal.Web.Tests.Unit/Services/Views/LabViews/LabViewServiceTests.Validations.Add.cs
+++ DMX.Portal.Web.Tests.Unit/Services/Views/LabViews/LabViewServiceTests.Validations.Add.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DMX.Portal.Web.Models.Labs;
 using DMX.Portal.Web.Models.Views.LabViews;
 using DMX.Portal.Web.Models.Views.LabViews.Exceptions;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -19,14 +20,61 @@ namespace DMX.Portal.Web.Tests.Unit.Services.Views.LabViews
             // given
             LabView nullLabView = null;
             var nullLabViewException = new NullLabViewException();
-            var expectedLabViewValidationException = new LabViewValidationException(nullLabViewException);
+            var expectedLabViewValidationException = 
+                new LabViewValidationException(nullLabViewException);
 
             // when
             ValueTask<LabView> addLabViewTask =
                 this.labViewService.AddLabViewAsync(nullLabView);
 
+            var actualLabViewValidationException =
+                await Assert.ThrowsAsync<LabViewValidationException>(addLabViewTask.AsTask);
+
             // then
-            await Assert.ThrowsAsync<LabViewValidationException>(addLabViewTask.AsTask);
+            actualLabViewValidationException.Should().BeEquivalentTo(expectedLabViewValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedLabViewValidationException))),
+                    Times.Once);
+
+            this.labServiceMock.Verify(service =>
+                service.AddLabAsync(It.IsAny<Lab>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.labServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfLabViewIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidLabView = new LabView()
+            {
+                Name = invalidString,
+                Description = invalidString,
+                DmxVersion = invalidString
+            };
+
+            var invalidLabViewException =
+                new InvalidLabViewException();
+
+            var expectedLabViewValidationException =
+                new LabViewValidationException(invalidLabViewException);
+
+            // when
+            ValueTask<LabView> addLabViewTask =
+                this.labViewService.AddLabViewAsync(invalidLabView);
+
+            var actualLabViewValidationException =
+                await Assert.ThrowsAsync<LabViewValidationException>(addLabViewTask.AsTask);
+
+            // then
+            actualLabViewValidationException.Should().BeEquivalentTo(expectedLabViewValidationException);
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(expectedLabViewValidationException))),
---
+++ DMX.Portal.Web/Models/Views/LabViews/Exceptions/InvalidLabViewException.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections;
+using Xeptions;
+
+namespace DMX.Portal.Web.Models.Views.LabViews.Exceptions
+{
+    public class InvalidLabViewException : Xeption
+    {
+        public InvalidLabViewException()
+            : base(message: "Invalid lab error occurred, please correct the errors and try again.")
+        { }
+
+        public InvalidLabViewException(Exception innerException, IDictionary data)
+            : base(message: "Invalid lab error occurred, please correct the errors and try again.",
+                  innerException,
+                  data)
+        { }
+    }
+}
