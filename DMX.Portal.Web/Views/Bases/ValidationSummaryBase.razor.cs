bff9bd1eb74d62e72a49e9cc64c64c5a8928a82c
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-27 10:34:02 -0700

    BASE: Initialize ValidationSummaryBase

---
+++ DMX.Portal.Web/Views/Bases/ValidationSummaryBase.razor.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public partial class ValidationSummaryBase : ComponentBase
+    {
+        [Parameter]
+        public IDictionary ValidationData { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        [Parameter]
+        public string Key { get; set; }
+
+        [Parameter]
+        public string Color { get; set; }
+
+        public IEnumerable<string> Errors
+        {
+            get => this.ValidationData?[Key] as IEnumerable<string>;
+            set => Errors = value;
+        }
+    }
+}
