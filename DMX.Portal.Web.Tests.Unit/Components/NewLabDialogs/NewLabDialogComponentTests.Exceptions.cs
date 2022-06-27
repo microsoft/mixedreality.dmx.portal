b6e55ce87716f454aae33946520c9222890f55cb
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-27 13:44:39 -0700

    ShouldRenderValidationDetailsOnLabAsync -> FAIL

---
+++ DMX.Portal.Web.Tests.Unit/Components/NewLabDialogs/NewLabDialogComponentTests.Exceptions.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Portal.Web.Models.Views.LabViews;
+using DMX.Portal.Web.Views.Components.NewLabDialogs;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.NewLabDialogs
+{
+    public partial class NewLabDialogComponentTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldRenderValidationDetailsOnLabAsync(
+            Xeption labViewValidationException)
+        {
+            // given
+            string someLabName = GetRandomString();
+            string someLabDescription = GetRandomString();
+
+            this.labViewServiceMock.Setup(service =>
+                service.AddLabViewAsync(It.IsAny<LabView>()))
+                    .ThrowsAsync(labViewValidationException);
+
+            // when
+            this.renderedNewLabDialog = 
+                RenderComponent<NewLabDialog>();
+
+            this.renderedNewLabDialog.Instance
+                .OpenDialog();
+
+            await this.renderedNewLabDialog.Instance.LabName
+                .SetValueAsync(someLabName);
+
+            await this.renderedNewLabDialog.Instance.LabDescription
+                .SetValueAsync(someLabDescription);
+
+            this.renderedNewLabDialog.Instance.Dialog
+                .Click();
+
+            // then
+            this.renderedNewLabDialog.Instance.Dialog.IsVisible
+                .Should().BeTrue();
+
+            this.renderedNewLabDialog.Instance.LabName.IsDisabled
+                .Should().BeFalse();
+
+            this.renderedNewLabDialog.Instance.LabDescription.IsDisabled
+                .Should().BeFalse();
+
+            this.renderedNewLabDialog.Instance.Dialog.DialogButton.Disabled
+                .Should().BeFalse();
+
+            this.renderedNewLabDialog.Instance.Spinner.IsVisible
+                .Should().BeFalse();
+
+            this.renderedNewLabDialog.Instance.ContentValidationSummary.ValidationData
+                .Should().BeEquivalentTo(
+                    labViewValidationException.InnerException.Data);
+
+            this.renderedNewLabDialog.Instance.ContentValidationSummary.Color
+                .Should().Be("Red");
+
+            this.labViewServiceMock.Verify(service =>
+                service.AddLabViewAsync(
+                    It.IsAny<LabView>()),
+                        Times.Once());
+
+            this.labViewServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- DMX.Portal.Web.Tests.Unit/Components/NewLabDialogs/NewLabDialogComponentTests.cs
+++ DMX.Portal.Web.Tests.Unit/Components/NewLabDialogs/NewLabDialogComponentTests.cs
@@ -2,13 +2,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
+using System.Linq;
 using Bunit;
+using DMX.Portal.Web.Models.Labs.Exceptions;
+using DMX.Portal.Web.Models.Views.LabViews;
+using DMX.Portal.Web.Models.Views.LabViews.Exceptions;
 using DMX.Portal.Web.Services.Views.LabViews;
 using DMX.Portal.Web.Views.Components.NewLabDialogs;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Syncfusion.Blazor;
 using Tynamix.ObjectFiller;
+using Xeptions;
+using Xunit;
 
 namespace DMX.Portal.Web.Tests.Unit.Components.NewLabDialogs
 {
@@ -25,10 +31,54 @@ namespace DMX.Portal.Web.Tests.Unit.Components.NewLabDialogs
             this.Services.AddOptions();
         }
 
-        public static string GetRandomString() =>
+        public static TheoryData<Xeption> DependencyValidationExceptions()
+        {
+            string[] randomLabViewNameErrorMessages = GetRandomErrorMessages();
+            string[] randomLabViewDescriptionErrorMessages = GetRandomErrorMessages();
+            var invalidLabViewNameException = new InvalidLabViewException();
+            var invalidLabViewDescriptionException = new InvalidLabViewException();
+            var invalidLabViewNameDescriptionException = new InvalidLabViewException();
+
+            invalidLabViewNameException.AddData(
+                key: nameof(LabView.Name),
+                values: randomLabViewNameErrorMessages);
+
+            invalidLabViewDescriptionException.AddData(
+                key: nameof(LabView.Description),
+                values: randomLabViewDescriptionErrorMessages);
+
+            invalidLabViewNameDescriptionException.AddData(
+                key: nameof(LabView.Name),
+                values: randomLabViewNameErrorMessages);
+
+            invalidLabViewNameDescriptionException.AddData(
+                key: nameof(LabView.Description),
+                values: randomLabViewDescriptionErrorMessages);
+
+            return new TheoryData<Xeption>
+            {
+                new LabViewValidationException(invalidLabViewNameException),
+                new LabViewDependencyException(invalidLabViewNameException),
+                new LabViewValidationException(invalidLabViewDescriptionException),
+                new LabViewDependencyException(invalidLabViewDescriptionException),
+                new LabViewValidationException(invalidLabViewNameDescriptionException),
+                new LabViewDependencyException(invalidLabViewNameDescriptionException)
+            };
+        }
+
+        private static string GetRandomString() =>
             new MnemonicString(GetRandomNumber()).GetValue();
 
-        public static int GetRandomNumber() =>
+        private static int GetRandomNumber() =>
             new IntRange(min: 1, max: 10).GetValue();
+
+        private static string[] GetRandomErrorMessages()
+        {
+            int randomCount = GetRandomNumber();
+
+            return Enumerable.Range(start: 0, count: randomCount)
+                .Select(item => GetRandomString())
+                    .ToArray();
+        }
     }
 }
