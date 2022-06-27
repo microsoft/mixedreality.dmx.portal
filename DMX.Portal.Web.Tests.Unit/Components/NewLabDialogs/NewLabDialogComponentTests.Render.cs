ef3a4aeb3e8f1fd8376142bba2797324b752df46
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-27 10:46:01 -0700

    ShouldRenderDefaults -> PASS

---
+++ DMX.Portal.Web.Tests.Unit/Components/NewLabDialogs/NewLabDialogComponentTests.Render.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Models.Views.Components.NewLabDialogComponents;
+using DMX.Portal.Web.Views.Components.NewLabDialogs;
+using FluentAssertions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.NewLabDialogs
+{
+    public partial class NewLabDialogComponentTests
+    {
+        [Fact]
+        public void ShouldRenderDefaults()
+        {
+            // given . when
+            var initialNewLabDialogComponent = new NewLabDialog();
+
+            // then
+            initialNewLabDialogComponent.LabViewService.Should().BeNull();
+            initialNewLabDialogComponent.State.Should().Be(NewLabDialogComponentState.Loading);
+            initialNewLabDialogComponent.Dialog.Should().BeNull();
+            initialNewLabDialogComponent.LabName.Should().BeNull();
+            initialNewLabDialogComponent.LabDescription.Should().BeNull();
+            initialNewLabDialogComponent.IsVisible.Should().BeFalse();
+            initialNewLabDialogComponent.LabView.Should().BeNull();
+            initialNewLabDialogComponent.Exception.Should().BeNull();
+            initialNewLabDialogComponent.ErrorMessage.Should().BeNull();
+            initialNewLabDialogComponent.Spinner.Should().BeNull();
+            initialNewLabDialogComponent.ContentValidationSummary.Should().BeNull();
+        }
+    }
+}
---
+++ DMX.Portal.Web.Tests.Unit/Components/NewLabDialogs/NewLabDialogComponentTests.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Bunit;
+using DMX.Portal.Web.Views.Components.NewLabDialogs;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.NewLabDialogs
+{
+    public partial class NewLabDialogComponentTests : TestContext
+    {
+        private IRenderedComponent<NewLabDialog> renderedNewLabDialog;
+    }
+}
