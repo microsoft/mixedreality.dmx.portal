783f350a37c6066121bc2f85be1d9272d7dceedb
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-29 08:12:46 -0700

    ShouldThrowValidationExceptionOnAddIfLabIsInvalidAndLogItAsync -> PASS

--- DMX.Portal.Web/Services/Foundations/Labs/LabService.Validations.cs
+++ DMX.Portal.Web/Services/Foundations/Labs/LabService.Validations.cs
@@ -16,6 +16,7 @@ namespace DMX.Portal.Web.Services.Foundations.Labs
 
             Validate(
                 (Rule: IsInvalid(lab.Id), Parameter: nameof(Lab.Id)),
+                (Rule: IsInvalidId(lab.ExternalId), Parameter: nameof(Lab.ExternalId)),
                 (Rule: IsInvalid(lab.Name), Parameter: nameof(Lab.Name)),
                 (Rule: IsInvalid(lab.Description), Parameter: nameof(Lab.Description)));
         }
@@ -31,6 +32,12 @@ namespace DMX.Portal.Web.Services.Foundations.Labs
         private static dynamic IsInvalid(Guid id) => new
         {
             Condition = id == Guid.Empty,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalidId(string id) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(id),
             Message = "Id is required"
         };
 
