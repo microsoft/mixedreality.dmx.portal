0d977a609856fc6213bf50ea4ea761fb1dd9dede
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-06-30 14:25:24 -0700

    BASE: Create StyleBase

---
+++ DMX.Portal.Web/Views/Bases/StyleBase.razor.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using SharpStyles.Models;
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public partial class StyleBase : ComponentBase
+    {
+        [Parameter]
+        public SharpStyle Style { get; set; }
+    }
+}
