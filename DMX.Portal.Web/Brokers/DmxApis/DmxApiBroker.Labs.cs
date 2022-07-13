ff628d6f769cdbc6175f7fc63613ab582c468b2f
Michael Mendelsohn <jazzapple42@gmail.com>
2022-07-13 11:25:04 -0700

    CODE RUB: Revert Dummy Data

--- DMX.Portal.Web/Brokers/DmxApis/DmxApiBroker.Labs.cs
+++ DMX.Portal.Web/Brokers/DmxApis/DmxApiBroker.Labs.cs
@@ -2,7 +2,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DMX.Portal.Web.Models.Labs;
@@ -14,34 +13,7 @@ namespace DMX.Portal.Web.Brokers.DmxApis
         private const string LabsRelativeUrl = "api/labs";
 
         public async ValueTask<List<Lab>> GetAllLabsAsync() =>
-            await new ValueTask<List<Lab>>(new List<Lab>
-            {
-                new Lab
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "My Lab",
-                    Devices = new List<LabDevice>
-                    {
-                        new LabDevice
-                        {
-                            Name = "Some Device"
-                        }
-                    }
-                },
-
-                new Lab
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "My Lab 2",
-                    Devices = new List<LabDevice>
-                    {
-                        new LabDevice
-                        {
-                            Name = "Some Device 2"
-                        }
-                    }
-                }
-            });
+            await GetAsync<List<Lab>>(LabsRelativeUrl);
 
         public async ValueTask<Lab> PostLabAsync(Lab lab) =>
             await PostAsync<Lab>(LabsRelativeUrl, lab);
