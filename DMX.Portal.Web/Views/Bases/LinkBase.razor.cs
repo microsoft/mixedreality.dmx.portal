f0fcc778e57324142091f0706e6d1e575cf359f1
Michael Mendelsohn <jazzapple42@gmail.com>
2022-07-18 15:56:33 -0700

    BASE: Add Link Base

---
+++ DMX.Portal.Web/Views/Bases/LinkBase.razor.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+namespace DMX.Portal.Web.Views.Bases
+{
+    public class LinkBase
+    {
+        public string ClassName { get; set; }
+        public string Uri { get; set; }
+        public string Text { get; set; }
+    }
+}
