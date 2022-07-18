dfdac7ad5ce6fe6ef9343a070add5e04162236bf
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-07-18 10:20:09 -0700

    ShouldRenderDefaultValues -> PASS

---
+++ DMX.Portal.Web.Tests.Unit/Components/Headers/HeaderComponentTests.Render.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Views.Components.Headers;
+using FluentAssertions;
+using Xunit;
+
+namespace DMX.Portal.Web.Tests.Unit.Components.Headers
+{
+    public partial class HeaderComponentTests
+    {
+        [Fact]
+        public void ShouldRenderDefaultValues()
+        {
+            // given . when
+            var renderedHeaderComponent = new HeaderComponent();
+
+            // then
+            renderedHeaderComponent.Header.Should().BeNull();
+        }
+    }
+}
---
+++ DMX.Portal.Web/Views/Components/Headers/HeaderComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using DMX.Portal.Web.Views.Bases;
+
+namespace DMX.Portal.Web.Views.Components.Headers
+{
+    public partial class HeaderComponent
+    {
+        public HeaderBase Header { get; set; }
+    }
+}
