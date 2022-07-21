b0a8f8d258427dc071d4d845cd0b1ca0fd7eb4c5
Michael Mendelsohn <jazzapple42@gmail.com>
2022-07-21 10:56:50 -0700

    PROVISION: Add WebApp Settings

--- DMX.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.WebApps.cs
+++ DMX.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.WebApps.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. 
 // ---------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.AppService.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
@@ -15,11 +16,27 @@ namespace DMX.Portal.Web.Infrastructure.Provision.Brokers.Clouds
             IAppServicePlan appServicePlan,
             IResourceGroup resourceGroup)
         {
+            var webAppSettings = new Dictionary<string, string>
+            {
+                { "ASPNETCORE_ENVIRONMENT", ProjectEnvironment },
+                { "ApiConfigurations:Url", this.dmxGatekeeperApiUrl},
+                { "ApiConfiguration:AccessKey", this.dmxGatekeeperApiAccessKey},
+                { "AzureAd:TenantId", this.tenantId},
+                { "AzureAd:Instance", this.dmxPortalInstance},
+                { "AzureAd:Domain", this.dmxPortalDomain},
+                { "AzureAd:ClientId", this.dmxPortalClientId },
+                { "AzureAd:CallbackPath", this.dmxPortalCallbackPath},
+                { "DownstreamApi:BaseUrl", this.dmxGatekeeperApiUri},
+                { "DownstreamApi:Scopes:GetAllLabs", this.dmxGatekeeperApiScopesGetAllLabs},
+                { "DownstreamApi:Scopes:PostLab", this.dmxGatekeeperApiScopesPostLab},
+            };
+
             return await azure.AppServices.WebApps
                 .Define(name: webAppName)
                 .WithExistingWindowsPlan(appServicePlan: appServicePlan)
                 .WithExistingResourceGroup(group: resourceGroup)
                 .WithNetFrameworkVersion(version: NetFrameworkVersion.Parse("v6.0"))
+                .WithAppSettings(settings: webAppSettings)
                 .CreateAsync();
         }
     }
