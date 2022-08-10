da398637d6156d71aeb9e3a0dcac2ab3bfc1eb8e
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-10 12:03:22 -0700

    BROKERS: Get DateTime

---
+++ DMX.Portal.Web/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// ---------------------------------------------------------------
+
+using System;
+
+namespace DMX.Portal.Web.Brokers.DateTimes
+{
+    public class DateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() => DateTimeOffset.UtcNow;
+    }
+}
